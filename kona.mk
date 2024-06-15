89042926c79cb31505346844c326929707652e9c
Alcatraz323 <alcatraz32323@gmail.com>
2024-06-15 22:34:47 +0800

    sm8250-common: overlay: reconstruct to full RRO
    Signed-off-by: Flicker372 <flicker372@outlook.com>

--- kona.mk
+++ kona.mk
@@ -277,9 +277,13 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+PRODUCT_PACKAGES += \
+    CarrierConfigCommonOverlay \
+    FrameworksCommonOverlay \
+    SettingsCommonOverlay \
+    SettingsProviderCommonOverlay \
+    SystemUICommonOverlay \
+    TelephonyCommonOverlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
@@ -384,7 +388,7 @@ PRODUCT_PACKAGES += \
     libwpa_client \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
-    WifiResCommon \
+    WifiCommonOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
 
