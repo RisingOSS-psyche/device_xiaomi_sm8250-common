ee7ecf04a2285a2e8e33f1616a45cfd6086f5b9c
Flicker372 <flicker372@outlook.com>
2024-11-13 12:30:14 +0800

    sm8250-common: Link libgui_shim to fix elf check for libwfdmmsrc_system.so
    * Fixed:
    out/soong/.intermediates/vendor/xiaomi/sm8250-common/libwfdmmsrc_system/android_arm64_armv8-2a-dotprod_cortex-a76_shared/2a518aec44df1d8c0885fe0f6322b792/libwfdmmsrc_system.so: error: Unresolved symbol: _ZN7android21SurfaceComposerClient13createDisplayERKNS_7String8Ebf
    out/soong/.intermediates/vendor/xiaomi/sm8250-common/libwfdmmsrc_system/android_arm64_armv8-2a-dotprod_cortex-a76_shared/2a518aec44df1d8c0885fe0f6322b792/libwfdmmsrc_system.so: error: Unresolved symbol: _ZN7android21SurfaceComposerClient14destroyDisplayERKNS_2spINS_7IBinderEEE
    
    ref:
    https://github.com/LineageOS/android_hardware_lineage_compat/commit/beb5c4e069902de231db9f2752e3dd7c139d1349
    
    Signed-off-by: Flicker372 <flicker372@outlook.com>

--- extract-files.sh
+++ extract-files.sh
@@ -78,6 +78,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
             ;;
+        system_ext/lib64/libwfdmmsrc_system.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+            ;;
         vendor/etc/init/init.mi_thermald.rc)
             [ "$2" = "" ] && return 0
             sed -i "/seclabel u:r:mi_thermald:s0/d" "${2}"
